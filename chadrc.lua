a8670609bb0f85af196c005775c07b24f6e21a53
David Lignell <davidlignell@byu.edu>
2023-11-20 08:43:05 -0700

    chadrc theme

--- chadrc.lua
+++ chadrc.lua
@@ -5,7 +5,7 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-    theme = "repent",
+    theme = "forsake",
     theme_toggle = { "repent", "forsake" },
 
     hl_override = highlights.override,
